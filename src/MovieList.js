a46437f6e3a7a2dec6c8125b7e8be2ff9f9b9f0e
G Porto <lunaticpoetlover@gmail.com>
2025-11-13 01:15:39 -0300

    separated components into different files, created StarRating component

---
+++ src/MovieList.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { tempMovieData } from "./App";
+import { Movie } from "./Movie";
+
+export function MovieList({ movies }) {
+  return (
+    <ul className="list">
+      {movies?.map((movie) => (
+        <Movie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
